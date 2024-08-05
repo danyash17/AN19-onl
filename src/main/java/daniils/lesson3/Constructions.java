0aaa2b54740e8ded7cddeee0a5e253824b3f1fdf
Daniil <danikshkabara@tut.by>
2024-08-05 17:31:32 +0300

    feat: prepare for lesson 3

---
+++ src/main/java/daniils/lesson3/Constructions.java
@@ -0,0 +1,9 @@
+package daniils.lesson3;
+
+public class Constructions {
+
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ src/main/java/daniils/lesson3/Literals.java
@@ -0,0 +1,36 @@
+package daniils.lesson3;
+
+public class Literals {
+
+    public static void main(String[] args) {
+        //Literals
+
+        //binary
+        int bin = 0b01;
+        System.out.println(bin); //1
+        //octal
+        int oct = 0111;
+        System.out.println(oct); //73
+        //decimal
+        int dec = 111;
+        System.out.println(dec); //111
+        //hex
+        int hex = 0xAF;
+        System.out.println(hex); //175
+
+        //Floating numbers
+
+        //What's the output of the below?
+        //System.out.println(0.1 + 0.2);
+
+        // Casting
+        byte num = (byte) 1000;
+        System.out.println(num);
+
+        //char zero = 42;
+        //System.out.println(zero);
+        //char minOne = -1;
+
+    }
+
+}
