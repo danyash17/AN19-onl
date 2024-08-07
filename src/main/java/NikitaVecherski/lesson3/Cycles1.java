b52e87c19caab3d1dc44b725ed5ce20116d7ddfe
vchr17 <vecherski.ni@gmail.com>
2024-08-07 14:23:12 +0300

    feat: done Cycles1

---
+++ src/main/java/NikitaVecherski/lesson3/Cycles1.java
@@ -0,0 +1,9 @@
+package NikitaVecherski.lesson3;
+
+public class Cycles1 {
+    public static void main(String[] args) {
+        for (int i = 1; i < 100; i++) {
+            System.out.println(i);
+        }
+    }
+}
