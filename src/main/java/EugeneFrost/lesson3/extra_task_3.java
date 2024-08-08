87caf8d07a3fc16eb8e7c6ccc82794a8fb56fa71
Eugene_Frost <angell.fr.hell@gmail.com>
2024-08-08 17:45:07 +0300

    feat: finished task

---
+++ src/main/java/EugeneFrost/lesson3/extra_task_3.java
@@ -0,0 +1,16 @@
+package EugeneFrost.lesson3;
+
+public class extra_task_3 {
+    public static void main(String[] args) {
+        int rows = 10;
+        int cols = 10;
+
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= cols; j++) {
+                int result=i*j;
+                System.out.println(i + " * " + j + " = " + result);
+            }
+            System.out.println("=============");
+        }
+    }
+}
