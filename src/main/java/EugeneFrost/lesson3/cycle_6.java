88675f2c6d6a71a7dc83cf273deaefa809d694f8
Eugene_Frost <angell.fr.hell@gmail.com>
2024-08-08 16:46:35 +0300

    feat: finished task

--- src/main/java/EugeneFrost/lesson3/cycle_6.java
+++ src/main/java/EugeneFrost/lesson3/cycle_6.java
@@ -1,4 +1,9 @@
 package EugeneFrost.lesson3;
 
 public class cycle_6 {
+    public static void main(String[] args) {
+        for (int i = 10; i <= 20; i++) {
+            System.out.print((i*i) + " ");
+        }
+    }
 }
