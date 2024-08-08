836bf7bdc9885ae5b1a1395b5b3ef6f156ac02d5
Monarch-90 <v.avetisov@list.ru>
2024-08-09 02:37:44 +0300

    feat: added lesson3_homework_cycle_summ

---
+++ src/main/java/vadimavetisov/lesson3/lesson3_homework_cycle_summ.java
@@ -0,0 +1,23 @@
+package vadimavetisov.lesson3;
+
+import java.util.Scanner;
+
+public class lesson3_homework_cycle_summ {
+    //3. Напишите программу, где пользователь вводит любое целое
+    //положительное число. А программа суммирует все числа от 1 до
+    //введенного пользователем числа.
+    //Для ввода числа воспользуйтесь классом Scanner.
+
+    public static void main(String[] args) {
+
+        System.out.println("Enter number: ");
+        Scanner s = new Scanner(System.in);
+        int num = s.nextInt();
+
+        int summ = 0;
+        for (int i = 1; i <= num; i++) {
+            summ = summ + i;
+        }
+        System.out.println(summ);
+    }
+}
