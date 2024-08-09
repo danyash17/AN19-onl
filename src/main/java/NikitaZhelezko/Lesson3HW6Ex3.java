9d6d035edc2005d614d37bae11362e6e6dfcf478
NickZhezl <zhelezko.nik12345@gmail.com>
2024-08-09 15:57:32 +0300

    feat: added if construction

--- src/main/java/NikitaZhelezko/Lesson3HW6Ex3.java
+++ src/main/java/NikitaZhelezko/Lesson3HW6Ex3.java
@@ -5,6 +5,9 @@ public class Lesson3HW6Ex3 {
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите любое число: ");
-        int i = scanner.nextInt();
+        int number = scanner.nextInt();
+        if (number<=0){
+            System.out.println("Пожалуйста, введите положительное число");
+        }
     }
 }
