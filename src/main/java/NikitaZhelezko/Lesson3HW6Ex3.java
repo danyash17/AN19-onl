ce94d4ca21d06be85cd6fb1cc69a501ed64f1922
NickZhezl <zhelezko.nik12345@gmail.com>
2024-08-09 15:50:02 +0300

    feat: added scanner

--- src/main/java/NikitaZhelezko/Lesson3HW6Ex3.java
+++ src/main/java/NikitaZhelezko/Lesson3HW6Ex3.java
@@ -1,5 +1,10 @@
 package NikitaZhelezko;
 //lesson 3 Homework 6 exercise 3 cikly
+import java.util.Scanner;
 public class Lesson3HW6Ex3 {
-
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите любое число: ");
+        int i = scanner.nextInt();
+    }
 }
