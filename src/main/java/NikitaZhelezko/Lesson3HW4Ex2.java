7e402970ccec0a4347b1b9de45debd5ad69be38f
NickZhezl <zhelezko.nik12345@gmail.com>
2024-08-09 15:37:01 +0300

    feat: added for construction

--- src/main/java/NikitaZhelezko/Lesson3HW4Ex2.java
+++ src/main/java/NikitaZhelezko/Lesson3HW4Ex2.java
@@ -1,5 +1,9 @@
 package NikitaZhelezko;
 //lesson 3 Homework 3 exercise 4 cikly
 public class Lesson3HW4Ex2 {
-
+    public static void main(String[] args){
+        for (int i = 5; i>0; i--){
+            System.out.println(i);
+        }
+    }
 }
