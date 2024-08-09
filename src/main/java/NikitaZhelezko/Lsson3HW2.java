88b71402137ddc2c8caa80b4b0563eff055787f1
NickZhezl <zhelezko.nik12345@gmail.com>
2024-08-09 15:04:05 +0300

    feat: added comment about exercise

--- src/main/java/NikitaZhelezko/Lsson3HW2.java
+++ src/main/java/NikitaZhelezko/Lsson3HW2.java
@@ -1,5 +1,5 @@
 package NikitaZhelezko;
-//lesson 3 Homework 2
+//lesson 3 Homework 2 exercise 3 upravl konstr
 import java.util.Scanner;
 public class Lsson3HW2 {
     public static void main(String[] args){
