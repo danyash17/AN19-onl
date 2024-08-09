96eae3bb81b8c013468ecce61fb6b8cedf1d87e1
NickZhezl <zhelezko.nik12345@gmail.com>
2024-08-09 15:14:36 +0300

    feat: added scanner

--- src/main/java/NikitaZhelezko/Lesson3HW3Ex4.java
+++ src/main/java/NikitaZhelezko/Lesson3HW3Ex4.java
@@ -1,4 +1,11 @@
 package NikitaZhelezko;
 //lesson 3 Homework 3 exercise 4 upravl konstr
+import java.util.Scanner;
 public class Lesson3HW3Ex4 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите значение температуры t: ");
+        int t = scanner.nextInt();
+
+    }
 }
