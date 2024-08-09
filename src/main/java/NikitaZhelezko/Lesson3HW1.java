89d2fd454488ea24722a0d58c5b39530bf31cac1
NickZhezl <zhelezko.nik12345@gmail.com>
2024-08-09 14:41:58 +0300

    feat: added scanner

--- src/main/java/NikitaZhelezko/Lesson3HW1.java
+++ src/main/java/NikitaZhelezko/Lesson3HW1.java
@@ -1,9 +1,13 @@
 package NikitaZhelezko;
 //lesson 3 Homework 1
 
+import java.util.Scanner;
+
 public class Lesson3HW1 {
     public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
         System.out.println("Unit 1");
-
+        System.out.println("Введите номер месяца (от 1 до 12)");
+        int month = scanner.nextInt();
     }
 }
