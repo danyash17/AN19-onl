1afe460a4c25d4c97bde004d6cea94b463390844
NickZhezl <zhelezko.nik12345@gmail.com>
2024-08-09 14:48:23 +0300

    feat: added switch-case construction

--- src/main/java/NikitaZhelezko/Lesson3HW1.java
+++ src/main/java/NikitaZhelezko/Lesson3HW1.java
@@ -9,5 +9,31 @@ public class Lesson3HW1 {
         System.out.println("Unit 1");
         System.out.println("Введите номер месяца (от 1 до 12)");
         int month = scanner.nextInt();
+            String season;
+            switch (month) {
+                case 12:
+                case 1:
+                case 2:
+                    season = "Зима";
+                    break;
+                case 3:
+                case 4:
+                case 5:
+                    season = "Весна";
+                    break;
+                case 6:
+                case 7:
+                case 8:
+                    season = "Лето";
+                    break;
+                case 9:
+                case 10:
+                case 11:
+                    season = "Осень";
+                    break;
+                default:
+                    season = "Неверно указан месяц";
+                    break;
+            }
     }
 }
