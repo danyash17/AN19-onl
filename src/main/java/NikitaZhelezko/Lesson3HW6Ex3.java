71d9be3e2e6c65323a97b4d288bd108ccc89b96f
NickZhezl <zhelezko.nik12345@gmail.com>
2024-08-09 15:59:00 +0300

    feat: added else with if cycle

--- src/main/java/NikitaZhelezko/Lesson3HW6Ex3.java
+++ src/main/java/NikitaZhelezko/Lesson3HW6Ex3.java
@@ -9,5 +9,12 @@ public class Lesson3HW6Ex3 {
         if (number<=0){
             System.out.println("Пожалуйста, введите положительное число");
         }
+        else {
+            int sum = 0;
+            for (int i=1; i<number; i++){
+                sum+=i;
+            }
+            System.out.println("Сумма чисел от 1 до "+number+ " равна: " + sum);
+        }
     }
 }
