12349395421386b337fe654c32c3c8878cd8147f
NickZhezl <zhelezko.nik12345@gmail.com>
2024-08-09 15:08:13 +0300

    feat: added if construction

--- src/main/java/NikitaZhelezko/Lsson3HW2.java
+++ src/main/java/NikitaZhelezko/Lsson3HW2.java
@@ -4,5 +4,16 @@ import java.util.Scanner;
 public class Lsson3HW2 {
     public static void main(String[] args){
     Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите любое число");
+    int num = scanner.nextInt();
+    if (num % 2 == 0) {
+        System.out.println("Число чётное");
+    }
+    else if (num == 0){
+        System.out.println("Введён 0");
+    }
+    else {
+        System.out.println("Нечётное");
+    }
     }
 }
