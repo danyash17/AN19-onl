19fe0811fce2f08d1e9cc89846d8ba2e2c95eea6
NickZhezl <zhelezko.nik12345@gmail.com>
2024-08-09 15:01:22 +0300

    feat: added scanner

--- src/main/java/NikitaZhelezko/Lsson3HW2.java
+++ src/main/java/NikitaZhelezko/Lsson3HW2.java
@@ -1,7 +1,8 @@
 package NikitaZhelezko;
-
+//lesson 3 Homework 2
+import java.util.Scanner;
 public class Lsson3HW2 {
     public static void main(String[] args){
-
+    Scanner scanner = new Scanner(System.in);
     }
 }
