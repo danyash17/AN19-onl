b8c2db0c2b8fba9939fc395f5da1a2e298dc1794
NickZhezl <zhelezko.nik12345@gmail.com>
2024-08-09 15:46:09 +0300

    feat: added while construction

--- src/main/java/NikitaZhelezko/Lesson3HW5Ex4.java
+++ src/main/java/NikitaZhelezko/Lesson3HW5Ex4.java
@@ -3,5 +3,9 @@ package NikitaZhelezko;
 public class Lesson3HW5Ex4 {
     public static void main(String[] args){
         int i = 0;
+        while (i<98){
+            i+=7;
+            System.out.println(i);
+        }
     }
 }
