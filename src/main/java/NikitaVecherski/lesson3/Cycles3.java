ab638b36d9e3a5a2ff9df60eb7884a5a1465611e
vchr17 <vecherski.ni@gmail.com>
2024-08-11 16:28:41 +0300

    feat: redone Cycles3

--- src/main/java/NikitaVecherski/lesson3/Cycles3.java
+++ src/main/java/NikitaVecherski/lesson3/Cycles3.java
@@ -11,10 +11,9 @@ public class Cycles3 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите положительное число");
-        String cycles = scanner.nextLine();
-        int cyc = Integer.parseInt(cycles);
+        int cyc = scanner.nextInt();
         int res =0;
-        for (int i=0; i<=cyc; i++ ){
+        for (int i=1; i<=cyc; i++ ){
         res = res+i;
        }
         System.out.println("Сумма ваших чисел = " + res);
