e8925c85d79dfb68d1ca74f709a8f30a0ee2768c
Alex <unflor@ya.ru>
2024-08-11 21:50:38 +0300

    feat: added Homework4

---
+++ src/main/java/Alex_P/lesson3/Homework4.java
@@ -0,0 +1,11 @@
+package Alex_P.lesson3;
+//задача 1 из раздела циклы
+public class Homework4 {
+    public static void main(String[] args) {
+    int i;
+        for (i = 0; i <= 99; i++){
+            if (i%2 == 0)
+                System.out.println(i);
+        }
+    }
+}
