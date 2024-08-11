2f272be92d1f802529910b88d6da3ab68a85eea1
Alex <unflor@ya.ru>
2024-08-11 21:30:38 +0300

    feat: added homework1 new

--- src/main/java/Alex_P/lesson3/Homework1.java
+++ src/main/java/Alex_P/lesson3/Homework1.java
@@ -1,7 +1,51 @@
 package Alex_P.lesson3;
 
+import java.util.Scanner;
+
 public class Homework1 {
     public static void main(String[] args) {
-        //homework
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter season number ");
+        int seasonNumber = input.nextInt();
+        switch (seasonNumber){
+            case (1):
+                System.out.println("winter");
+                break;
+            case (2):
+                System.out.println("winter");
+                break;
+            case (3):
+                System.out.println("spring");
+                break;
+            case (4):
+                System.out.println("spring");
+                break;
+            case (5):
+                System.out.println("spring");
+                break;
+            case (6):
+                System.out.println("summer");
+                break;
+            case (7):
+                System.out.println("summer");
+                break;
+            case (8):
+                System.out.println("summer");
+                break;
+            case (9):
+                System.out.println("autumn");
+                break;
+            case (10):
+                System.out.println("autumn");
+                break;
+            case (11):
+                System.out.println("autumn");
+                break;
+            case (12):
+                System.out.println("winter");
+                break;
+
+        }
+        //System.out.println("Season number "+ seasonNumber);
     }
 }
