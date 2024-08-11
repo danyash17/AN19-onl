bacd02242fe008ef74915ebd02ee6eb253b96ccf
vchr17 <vecherski.ni@gmail.com>
2024-08-11 16:15:37 +0300

    feat: redone Construction3

--- src/main/java/NikitaVecherski/lesson3/Construction3.java
+++ src/main/java/NikitaVecherski/lesson3/Construction3.java
@@ -10,9 +10,7 @@ public class Construction3 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите четное или нечетное число");
-        String number = scanner.nextLine();
-
-        int n = Integer.parseInt(number);
+        int n = scanner.nextInt();
         if (n % 2 == 0){
             System.out.println("Число является чётным");
         }
