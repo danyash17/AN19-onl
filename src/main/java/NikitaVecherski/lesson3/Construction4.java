af434297f7e1832d28d648c179e02da0de6bd8d7
vchr17 <vecherski.ni@gmail.com>
2024-08-11 16:17:52 +0300

    feat: redone Construction4

--- src/main/java/NikitaVecherski/lesson3/Construction4.java
+++ src/main/java/NikitaVecherski/lesson3/Construction4.java
@@ -11,17 +11,19 @@ public class Construction4 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите температуру");
-        String temperature = scanner.nextLine();
-        int t = Integer.parseInt(temperature);
-        if (t>-5) {
+        int t = scanner.nextInt();
+        if (t > -5) {
             System.out.println("На улице тепло");
         }
-            if (-5>=t && t >-20) {
-                System.out.println("На улице нормально");
-            }
-            if (-20>=t){
-                    System.out.println("На улице холодно");
-                }
-            }
+
+        if (-5 >= t && t > -20) {
+            System.out.println("На улице нормально");
         }
+
+        if (-20 >= t) {
+            System.out.println("На улице холодно");
+        }
+
+    }
+}
 
