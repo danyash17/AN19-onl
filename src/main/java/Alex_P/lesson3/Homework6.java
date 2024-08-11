9cb1f3ac68f0a095286ad4ba1c35f681d94df523
Alex <unflor@ya.ru>
2024-08-11 22:04:53 +0300

    feat: added Homework6

---
+++ src/main/java/Alex_P/lesson3/Homework6.java
@@ -0,0 +1,14 @@
+package Alex_P.lesson3;
+
+public class Homework6 {
+    public static void main(String[] args) {
+        int firstNumber = 7;
+        int lastNumber = 98;
+        int Sum = 0;
+
+        while (Sum < lastNumber){
+            Sum += firstNumber;
+            System.out.println(Sum);
+        }
+    }
+}
