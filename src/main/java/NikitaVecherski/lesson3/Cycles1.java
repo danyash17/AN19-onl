feacfd84c285b1b14c8f15bec92b02282cf5a70d
vchr17 <vecherski.ni@gmail.com>
2024-08-11 16:27:25 +0300

    feat: redone Cycles1

--- src/main/java/NikitaVecherski/lesson3/Cycles1.java
+++ src/main/java/NikitaVecherski/lesson3/Cycles1.java
@@ -6,6 +6,8 @@ package NikitaVecherski.lesson3;
 public class Cycles1 {
     public static void main(String[] args) {
         for (int i = 1; i < 100; i++) {
+            if (i % 2 ==0)
+                continue;
             System.out.println(i);
         }
     }
