a305c231e3fedc2b4aa86ae7fc9676f7332f6ac7
Ivan <mazkovania@gmail.com>
2024-08-12 11:59:41 +0300

    feat : made Cycles5

---
+++ src/main/java/ivan_m/Cycles5.java
@@ -0,0 +1,12 @@
+package ivan_m;
+
+public class Cycles5 {
+    public static void main(String[] args) {
+        int number = 0;
+        for (int i = 0; i < 10; i++)
+        {
+            System.out.println(number);
+            number -= 5;
+        }
+    }
+}
