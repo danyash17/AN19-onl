349ce68900fd9c1f49a54f73f5d0a7f7a5fc3f57
Eugene_Frost <angell.fr.hell@gmail.com>
2024-08-13 01:10:30 +0300

    feat: task finished

---
+++ src/main/java/EugeneFrost/lesson5/ExtraDynamicArray_3.java
@@ -0,0 +1,29 @@
+package EugeneFrost.lesson5;
+
+//**task**
+//Создайте двумерный массив целых чисел. Выведите на консоль сумму всех
+//элементов массива.
+
+import java.util.Arrays;
+
+public class ExtraDynamicArray_3 {
+    public static void main(String[] args) {
+        int[][] array = new int[4][4];
+        int sum = 0;
+
+        for (int i = 0; i < array.length; i++) {
+            for (int j =0;j <array[i].length; j++) {
+                array[i][j] =(int) (Math.random() * 15);
+            }
+        }
+
+        System.out.println("Исходный массив: "+ Arrays.deepToString(array));
+
+        for (int i =0;i < array.length; i++) {
+            for (int j = 0;j < array[i].length;j++) {
+                sum += array[i][j];
+            }
+        }
+        System.out.print("Сумма всех элементов массива: "+sum);
+    }
+}
