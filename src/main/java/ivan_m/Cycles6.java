d5682385e51f58e170b2780e3306c9abd545a4a3
Ivan <mazkovania@gmail.com>
2024-08-12 12:07:38 +0300

    feat : made Cycles6

---
+++ src/main/java/ivan_m/Cycles6.java
@@ -0,0 +1,13 @@
+package ivan_m;
+
+public class Cycles6 {
+    public static void main(String[] args) {
+        int number = 10;
+        while (number < 21)
+        {
+            System.out.println(Math.pow(number, 2));
+            number++;
+        }
+    }
+
+}
