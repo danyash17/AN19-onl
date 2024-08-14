9aa39fa011445e2e2cf75b2c1ab4c88712c43b75
Monarch-90 <v.avetisov@list.ru>
2024-08-14 10:44:28 +0300

    refactor: added lesson3_homework2 - Ctrl+Alt+L

--- src/main/java/vadimavetisov/lesson3/lesson3_homework2.java
+++ src/main/java/vadimavetisov/lesson3/lesson3_homework2.java
@@ -12,19 +12,15 @@ public class lesson3_homework2 {
         Scanner s = new Scanner(System.in);
         int inputInt = s.nextInt();
 
-        if (inputInt==12||inputInt==1||inputInt==2) {
+        if (inputInt == 12 || inputInt == 1 || inputInt == 2) {
             System.out.println("Winter");
-        }
-        else if (inputInt==3||inputInt==4||inputInt==5) {
+        } else if (inputInt == 3 || inputInt == 4 || inputInt == 5) {
             System.out.println("Spring");
-        }
-        else if (inputInt==6||inputInt==7||inputInt==8) {
+        } else if (inputInt == 6 || inputInt == 7 || inputInt == 8) {
             System.out.println("Summer");
-        }
-        else if (inputInt==9||inputInt==10||inputInt==11) {
+        } else if (inputInt == 9 || inputInt == 10 || inputInt == 11) {
             System.out.println("Autumn");
-        }
-        else {
+        } else {
             System.out.println("Incorrectly! Please use numbers from 1 to 12");
         }
     }
