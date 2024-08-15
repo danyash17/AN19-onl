56f125eae21b77ca2ca46773d268ea3784a358bd
vchr17 <vecherski.ni@gmail.com>
2024-08-15 18:40:49 +0300

    fix : ODArray2 redone

--- src/main/java/NikitaVecherski/lesson4/ODArray2.java
+++ src/main/java/NikitaVecherski/lesson4/ODArray2.java
@@ -1,9 +1,7 @@
 package NikitaVecherski.lesson4;
 
 import java.util.Arrays;
-import java.util.OptionalDouble;
 import java.util.Scanner;
-import java.util.stream.DoubleStream;
 
 //2. Создайте и заполните массив случайным числами и выведете
 //максимальное, минимальное и среднее значение.
@@ -19,28 +17,20 @@ public class ODArray2 {
         if (n < 0) {
             System.out.println("Нельзя создать массив с отрицательным кол-вом секций");
         } else {
-            double[] ArrayRandom = new double[n];
+            double[] arrayrandom = new double[n];
             for (int i = 0; i < n; i++) {
-                ArrayRandom[i] = Math.random();
+                arrayrandom[i] = Math.random();
             }
+            Arrays.sort(arrayrandom);
+            String aftersort = Arrays.toString(arrayrandom);
+            System.out.println("Ваш массив: \n" + aftersort);
+            double min = arrayrandom[0];
+            double max = arrayrandom[n-1];
+            double avarage = (max - min)/2 + min;
 
-
-            String max =(DoubleStream.of(ArrayRandom).max() + " Является максимальным значением");
-            max = max.substring(14);
-
-            String avarage = (DoubleStream.of(ArrayRandom).average() + " Является средним значением");
-            avarage = avarage.substring(14);
-
-            String min = (DoubleStream.of(ArrayRandom).min() + " Является минимальным значением");
-            min = min.substring(14);
-
-            System.out.println(max);
-            System.out.println(avarage);
-            System.out.println(min);
-
-
+            System.out.println("Минимальное число в массиве: " + min);
+            System.out.println("Среднее число в массиве: " + avarage);
+            System.out.println("Максимальное число в массиве: " + max);
         }
     }
 }
-// к сожалению, не нашел как убрать OptionalDouble из вывода, toString не помог :/
-// ладно, я нашел как схитрить :)
