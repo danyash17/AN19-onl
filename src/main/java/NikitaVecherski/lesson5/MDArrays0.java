6e98b721d1e8772408e611e0368d0531afe52168
vchr17 <vecherski.ni@gmail.com>
2024-08-16 15:54:40 +0300

    feat: done MDArrays0 2.0v

--- src/main/java/NikitaVecherski/lesson5/MDArrays0.java
+++ src/main/java/NikitaVecherski/lesson5/MDArrays0.java
@@ -5,41 +5,72 @@ package NikitaVecherski.lesson5;
 //заданное число. Пусть число, на которое будет увеличиваться каждый элемент,
 //задается из консоли
 
+// Я несколько усложнил задачу, для отработки навыков из прошлых тем, добавив ввод размера массива и рандомное заполнение
+
 
 import java.util.Arrays;
+import java.util.Random;
 import java.util.Scanner;
 
 public class MDArrays0 {
     public static void main(String[] args) {
-        int strng = 3;
-        int cllr = 3;
-        int dpth = 2;
+        System.out.println("Введите количество строк трехмерного массива");
 
-        // на случай, если захочу модифицировать код для возможности ввода произвольного размера трехмерного массива
-
-        int[][][] matrix = new int[strng][cllr][dpth];
-        System.out.println(Arrays.deepToString(matrix) + " - Массив до изменений");
-
-        Scanner number = new Scanner(System.in);
-        System.out.println("Введите число, на которое нужно увеличить значения в массиве");
-        int num = number.nextInt();
-        if (num < 0) {
-            System.out.println("Нельзя увеличить отрицательным числом");
+        Scanner scanner = new Scanner(System.in);
+        int strng = scanner.nextInt();
+        if (strng <= 0) {
+            System.out.println("Кол-во строк не может быть менее 1");
         } else {
-            int[][][] matrixsum = matrix;
-            for (int c=0; c < dpth; c++) {
-                for (int b=0; b < cllr; b++) {
-                    for (int a=0; a < strng; a++) {
-                        matrixsum[a][b][c] = matrix[a][b][c] + num;
+
+            System.out.println("Введите количество колонок массива");
+            int cllr = scanner.nextInt();
+            if (cllr <= 0) {
+                System.out.println("Количество колонок массива не может быть менее 1");
+            } else {
+
+                System.out.println("Введите вложенность массива");
+                int dpth = scanner.nextInt();
+                if (dpth <= 1) {
+                    System.out.println("Вложенность трехмерного массива не может быть менее 2");
+                } else {
+
+                    int[][][] matrix = new int[strng][cllr][dpth];
+                    Random random = new Random();
+
+                    for (int c = 0; c < dpth; c++) {
+                        for (int b = 0; b < cllr; b++) {
+                            for (int a = 0; a < strng; a++) {
+                                matrix[a][b][c] = matrix[a][b][c] + random.nextInt(100);
+                            }
+
+                        }
+
                     }
 
+                    System.out.println(Arrays.deepToString(matrix) + " - Массив до изменений");
+
+
+                    System.out.println("Введите число, на которое нужно увеличить значения в массиве");
+                    int num = scanner.nextInt();
+                    if (num < 0) {
+                        System.out.println("Нельзя увеличить отрицательным числом");
+                    } else {
+                        int[][][] sum = matrix;
+                        for (int c = 0; c < dpth; c++) {
+                            for (int b = 0; b < cllr; b++) {
+                                for (int a = 0; a < strng; a++) {
+                                    sum[a][b][c] = matrix[a][b][c] + num;
+                                }
+
+                            }
+
+                        }
+                        System.out.println("Массив после прибавления: \n" + Arrays.deepToString(sum));
+
+
+                    }
                 }
-
             }
-            System.out.println(Arrays.deepToString(matrixsum));
-
-
         }
     }
 }
-
