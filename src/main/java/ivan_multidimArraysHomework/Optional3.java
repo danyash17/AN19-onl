f0a9dbbf07d12c83037fc4bc7a4566b0f5853786
Ivan <mazkovania@gmail.com>
2024-08-17 20:40:30 +0300

    fix : renamed from Optional2 to Optional3

--- src/main/java/ivan_multidimArraysHomework/Optional3.java
+++ src/main/java/ivan_multidimArraysHomework/Optional3.java
@@ -5,7 +5,7 @@ package ivan_multidimArraysHomework;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class Optional2 {
+public class Optional3 {
     public static void main(String[] args) {
         int rows, columns;
         int sum = 0;
