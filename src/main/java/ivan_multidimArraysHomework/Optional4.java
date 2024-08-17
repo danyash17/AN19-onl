ca90aa12d64479c160489d8468db7c820cda07c8
Ivan <mazkovania@gmail.com>
2024-08-17 20:38:58 +0300

    feat : made multidimOptional4

---
+++ src/main/java/ivan_multidimArraysHomework/Optional4.java
@@ -0,0 +1,52 @@
+//Создайте двумерный массив. Выведите на консоль диагонали массива.
+package ivan_multidimArraysHomework;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Optional4 {
+    public static void main(String[] args) {
+        int size;
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Введите размер матрицы:");
+        size = scanner.nextInt();
+        int[][] array = new int[size][size];
+
+        for (int i = 0; i < size; i++)
+        {
+            for (int j = 0; j < size; j++)
+            {
+                System.out.println("Введите [" + (i+1) + "][" + (j+1) + "]" + " элемент:");
+                array[i][j] = scanner.nextInt();
+            }
+        }
+        System.out.println("Матрица:");
+        for (int i = 0; i < size; i++)
+        {
+            for (int j = 0; j < size; j++)
+            {
+                System.out.print(array[i][j] + " ");
+            }
+            System.out.print("\n");
+        }
+
+        System.out.print("Главная диагональ : ");
+        for (int i = 0; i < size; i++)
+        {
+            for (int j = 0; j < size; j++)
+            {
+                if (i == j)
+                {
+                    System.out.print(array[i][j] + " ");
+                }
+            }
+        }
+        System.out.println();
+        System.out.print("Побочная диагональ : ");
+        for (int i = 0, j = size - 1; i < size; i++, j--)
+        {
+            System.out.print(array[i][j] + " ");
+        }
+    }
+}
