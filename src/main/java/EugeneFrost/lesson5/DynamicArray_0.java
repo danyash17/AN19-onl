b961f65c46f2f92d45ea7781d6a84e9dde56b066
Eugene_Frost <angell.fr.hell@gmail.com>
2024-08-18 15:10:41 +0300

    feat: changed

--- src/main/java/EugeneFrost/lesson5/DynamicArray_0.java
+++ src/main/java/EugeneFrost/lesson5/DynamicArray_0.java
@@ -31,7 +31,7 @@ public class DynamicArray_0 {
         for (int i = 0; i < 3; i++) {
             for (int j = 0; j < 3; j++) {
                 for (int k = 0; k < 3; k++) {
-                    arr[i][j][k] += 3;
+                    arr[i][j][k] += n;
                 }
             }
         }
