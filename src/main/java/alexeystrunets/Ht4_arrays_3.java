14386eaaeb9947d1d87653098ff21768f197fce6
alekseystrunets <145933314+alekseystrunets@users.noreply.github.com>
2024-08-18 16:50:07 +0300

    fix: <Homework from topic 4 under number3>:Calculated the arithmetic mean

--- src/main/java/alexeystrunets/Ht4_arrays_3.java
+++ src/main/java/alexeystrunets/Ht4_arrays_3.java
@@ -2,28 +2,31 @@ package alexeystrunets;
 
 import java.util.Arrays;
 
-public class ht4_arrays_3 {
+public class Ht4_arrays_3 {
     public static void main(String[] args) {
-        int[] arr1 = {1,6,3,2,2};
-        int[] arr2 = {1,6,3,2,2};
+        int[] arr1 = {1, 6, 3, 2, 2};
+        int[] arr2 = {1, 6, 3, 2, 2};
         int sum1 = 0;
-        double arif1 =0;
         int sum2 = 0;
-        double arif2 =0;
-        System.out.println("Первый массив: "+ Arrays.toString(arr1));
-        System.out.println("Второй массив: "+Arrays.toString(arr2));
-        for (int i=0;i<=arr1.length-1;i++) {
+        double arif1;
+        double arif2;
+
+        System.out.println("Первый массив: " + Arrays.toString(arr1));
+        System.out.println("Второй массив: " + Arrays.toString(arr2));
+
+        for (int i = 0; i < arr1.length; i++) {
             sum1 += arr1[i];
-            arif1 = sum1 / arr1.length;
             sum2 += arr2[i];
-            arif2 = sum2 / arr1.length;
         }
-        if (arif1>arif2){
+
+        arif1 = (double) sum1 / arr1.length;
+        arif2 = (double) sum2 / arr2.length;
+
+        if (arif1 > arif2) {
             System.out.println("Среднее арифметическое элементов первого массива больше второго.");
-        } else if (arif1<arif2) {
+        } else if (arif1 < arif2) {
             System.out.println("Среднее арифметическое элементов первого массива меньше второго.");
-        }
-        else {
+        } else {
             System.out.println("Среднее арифметическое элементов первого массива, равно среднему арифметическому второго.");
         }
     }
