4ed8a5620483fe821fc07b7f720ba86d411c94d5
alekseystrunets <145933314+alekseystrunets@users.noreply.github.com>
2024-08-18 21:42:40 +0300

    fix: <Homework from topic 5 under number4(Additionally)>:fix diagonal

--- src/main/java/alexeystrunets/Ht5_matrix_dop4.java
+++ src/main/java/alexeystrunets/Ht5_matrix_dop4.java
@@ -1,21 +1,33 @@
+
+//Создайте двумерный массив. Выведите на консоль диагонали массива
+
 package alexeystrunets;
+
 
 public class Ht5_matrix_dop4 {
     public static void main(String[] args) {
-        int[][] array = {
+        int[][] matrix = {
                 {1, 2, 3},
                 {4, 5, 6},
                 {7, 8, 9}
         };
-        int[][] diagonalArray = new int[array.length][array.length];
-        for (int i = 0; i < array.length; i++) {
-            diagonalArray[i][i] = array[i][i];
-        }
-
-        System.out.println("Массив с диагональными элементами:");
-        for (int[] row : diagonalArray) {
-            for (int column : row) {
-                System.out.print(column + " ");
+        int mainDiagonal, secondaryDiagonal;
+        System.out.println("Диагональные элементы:");
+        for (int i = 0; i < matrix.length; i++) {
+            for (int j = 0; j < matrix[i].length; j++) {
+                if (i == j) {
+                    mainDiagonal = matrix[i][j];
+                    System.out.print(mainDiagonal + " ");
+                }
+                // Помаялся и увидел закономерность на пообочной диагонали:
+                // для i и j меняются координаты зеркально(0, 1, 2 и 2, 1, 0) соответсвенно.
+                else if (j == matrix.length - 1 - i) {
+                    secondaryDiagonal = matrix[i][j];
+                    System.out.print(secondaryDiagonal + " ");
+                }
+                else {
+                    System.out.print("0 ");
+                }
             }
             System.out.println();
         }
