9d6a987c18b8d866fbda97eb00f63537d2a3d6b7
artembelonozhko <ar.belonozhko@gmail.com>
2024-08-19 19:14:23 +0300

    feat: homework lesson 4 part 2

--- src/main/java/daniil_s/Sandbox.java
+++ src/main/java/daniil_s/Sandbox.java
@@ -4,13 +4,5 @@ import artem.homework.Circle;
 
 public class Sandbox {
 
-    public static void main(String[] args) {
-
-        Circle circle = new Circle(3);
-        circle.getDiametr();
-
-        System.out.println(circle.getDiametr());
-
-    }
 
 }
