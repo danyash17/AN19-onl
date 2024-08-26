52e1bc3352fc86fc5d3c12e69ef8be53d37b8e47
Alex <unflor@ya.ru>
2024-08-26 17:02:47 +0300

    feat: added Homework 7 rework 2

--- src/main/java/Alex_P/lesson7/Dantist.java
+++ src/main/java/Alex_P/lesson7/Dantist.java
@@ -3,6 +3,6 @@ package Alex_P.lesson7;
 public class Dantist implements Doctor {
     @Override
     public void treat() {
-        System.out.println("Лечит дантист");;
+        System.out.println("Лечит дантист");
     }
 }
--- src/main/java/Alex_P/lesson7/Doctor.java
+++ src/main/java/Alex_P/lesson7/Doctor.java
@@ -1,5 +1,5 @@
 package Alex_P.lesson7;
 
 public interface Doctor {
-    public void treat();
+    void treat();
 }
