96ebe26006a74ab59d87a0de699c2a4ebf1049a0
alekseystrunets <145933314+alekseystrunets@users.noreply.github.com>
2024-08-27 22:59:04 +0300

    feat:<Ht7>:add task from dop

---
+++ src/main/java/alexeystrunets/ht7/Animal.java
@@ -0,0 +1,12 @@
+package alexeystrunets.ht7;
+
+public class Animal {
+    protected String typeOfAnimal;
+
+    public Animal(String typeOfAnimal){
+        this.typeOfAnimal = typeOfAnimal;
+    }
+    public void makeSound (){
+        System.out.println("Something");
+    }
+}
---
+++ src/main/java/alexeystrunets/ht7/Dog.java
@@ -0,0 +1,13 @@
+package alexeystrunets.ht7;
+
+public class Dog extends Animal{
+
+    public Dog(String typeOfAnimal){
+        super(typeOfAnimal);
+    }
+
+    @Override
+    public void makeSound() {
+        System.out.println("Ooh");
+    }
+}
