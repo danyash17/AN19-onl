135b262606223653d16e7a2b0e3849294441690c
Daniil <danikshkabara@tut.by>
2024-08-28 22:40:31 +0300

    feat: add lesson10 notes

---
+++ src/main/java/daniils/lesson10/Simulation.java
@@ -0,0 +1,18 @@
+package daniils.lesson10;
+
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+public class Simulation {
+
+    public static void main(String[] args) {
+        String hello = "Hello Alexey, nice to meet you";
+        String params = "";
+        String a = "abc";
+        String b = "abd";
+        System.out.println(a.compareTo(b));
+        System.out.println();\
+    }
+
+}
