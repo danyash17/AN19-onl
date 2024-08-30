bd62a6cf0696f9c6b897568c01812ecd6f8ada04
Ivan <mazkovania@gmail.com>
2024-08-30 11:54:48 +0300

    feat: made Doctor (lesson7)

---
+++ src/main/java/ivan_m/PillarsOfOOP/Doctor/Dentist.java
@@ -0,0 +1,8 @@
+package ivan_m.PillarsOfOOP.Doctor;
+
+public class Dentist extends Doctor {
+    @Override
+    public String cure() {
+        return "Привет! Я дантист. Будем лечить зубы";
+    }
+}
