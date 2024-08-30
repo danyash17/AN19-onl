a8fb77920bbbe4cd7d49251657d7b821cdcdd382
Ivan <mazkovania@gmail.com>
2024-08-30 17:36:22 +0300

    feat: made GameCharacter (from notes)

---
+++ src/main/java/ivan_m/PillarsOfOOP/GameCharacter/Mage.java
@@ -0,0 +1,8 @@
+package ivan_m.PillarsOfOOP.GameCharacter;
+
+public class Mage extends GameCharacter {
+    @Override
+    String attack() {
+        return "I am a mage! I will put a spell on you!";
+    }
+}
---
+++ src/main/java/ivan_m/PillarsOfOOP/GameCharacter/Warrior.java
@@ -0,0 +1,8 @@
+package ivan_m.PillarsOfOOP.GameCharacter;
+
+public class Warrior extends GameCharacter {
+    @Override
+    String attack() {
+        return "I am a warrior! I will kill you!";
+    }
+}
