7ec7c5e4d1ecdc3afcdd20876159a6155c47f9a0
NickZhezl <zhelezko.nik12345@gmail.com>
2024-09-08 21:08:30 +0300

    feat: add custom exception

---
+++ src/main/java/NikitaZhelezko/lesson11/homework/BookNotFoundException.java
@@ -0,0 +1,7 @@
+package NikitaZhelezko.lesson11.homework;
+
+public class BookNotFoundException extends Exception {
+    public BookNotFoundException(String message){
+        super(message);
+    }
+}
