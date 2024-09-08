ebd44f7029377fe2f36ac65a50b3f98dc4006e5c
EugeneFr0st <angell.fr.hell@gmail.com>
2024-09-08 17:10:40 +0300

    feat: updated

--- src/main/java/EugeneFrost/lesson9/TaskLibrary/BookNotFoundException.java
+++ src/main/java/EugeneFrost/lesson9/TaskLibrary/BookNotFoundException.java
@@ -2,7 +2,7 @@ package EugeneFrost.lesson9.TaskLibrary;
 
 import java.io.FileNotFoundException;
 
-public class BookNotFoundException extends FileNotFoundException {
+public class BookNotFoundException extends RuntimeException {
     public BookNotFoundException() {
         super("Книга не найдена!");
     }
