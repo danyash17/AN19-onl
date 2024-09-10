b16f5331c35331e78079ef9eb1f69146b6f4417b
Monarch-90 <v.avetisov@list.ru>
2024-09-10 18:58:45 +0300

    feat: lesson11/Library

---
+++ src/main/java/vadimavetisov/lesson11/BookDuplicatesException.java
@@ -0,0 +1,7 @@
+package vadimavetisov.lesson11;
+
+public class BookDuplicatesException extends RuntimeException{
+    public BookDuplicatesException(String message) {
+        super(message);
+    }
+}
