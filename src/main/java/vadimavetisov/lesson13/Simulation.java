3582e4238af10f9839552fbf41254996101aca07
Monarch-90 <v.avetisov@list.ru>
2024-09-11 17:16:31 +0300

    feat: lesson11/Library

---
+++ src/main/java/vadimavetisov/lesson13/Simulation.java
@@ -0,0 +1,15 @@
+package vadimavetisov.lesson13;
+
+public class Simulation {
+
+    public static void main(String[] args) {
+
+        System.out.println(StreamAPI.evenNum());
+        System.out.println(StreamAPI.spaces());
+        System.out.println(StreamAPI.sum());
+        System.out.println(StreamAPI.upperCase());
+        System.out.println(StreamAPI.maxNum());
+        System.out.println(StreamAPI.reviews());
+
+    }
+}
