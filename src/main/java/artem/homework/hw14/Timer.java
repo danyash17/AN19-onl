06dc61be228a555a45ee9fd12d7be648efdb5734
artembelonozhko <ar.belonozhko@gmail.com>
2024-09-13 13:00:50 +0300

    feat: homework lesson 14

---
+++ src/main/java/artem/homework/hw14/Timer.java
@@ -0,0 +1,26 @@
+package artem.homework.hw14;
+
+//Описание: Создайте поток, который с помощью цикла каждую
+//секунду уменьшает значение переменной с 10 до 0, и выводит
+//его на экран. В конце итерации цикла заставьте поток "уснуть"
+//на 1 секунду. Также, выведите "Расчет окончен" в конце метода main,
+//это сообщение должно быть последним сообщением программы.
+
+public class Timer {
+    public static void main(String[] args) {
+        Thread counterThread = new Thread(() -> {
+            int count = 10;
+            while (count >= 0) {
+                System.out.println("Count: " + count);
+                count--;
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            System.out.println("Расчет окончен");
+        });
+        counterThread.start();
+    }
+}
